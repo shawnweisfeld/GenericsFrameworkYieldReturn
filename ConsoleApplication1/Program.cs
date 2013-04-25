d02c4b602488cd0f7bd5734468551a69f83fd998
Shawn Weisfeld <shawn@shawnweisfeld.com>
2013-04-25 15:25:46 -0500

    Initial Commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class Tester
+    {
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = new B().GetTs<Tester>().First();
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+    }
+
+    public class A
+    {
+        public virtual IEnumerable<T> GetTs<T>()
+            where T : new()
+        {
+            return new T[] { new T() };
+        }
+    }
+
+    public class B : A
+    {
+        public override IEnumerable<T> GetTs<T>()
+        {
+            foreach (var t in base.GetTs<T>())
+            {
+                yield return t;
+            }
+        }
+    } 
+
+}
