5d83cdad1bc66e07f60e6a9bc289564764e82dfc
Shawn Weisfeld <shawn@shawnweisfeld.com>
2013-05-07 11:06:26 -0500

    Implement in VB

---
+++ ConsoleApplication2/Module1.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim b = New B().GetTs(Of Tester)().First()
+        Console.WriteLine("Done in VB!")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
+
+Public Class A
+
+    Public Overridable Function GetTs(Of t As {New})() As IEnumerable(Of t)
+        Return New t() {New t}
+    End Function
+
+End Class
+
+Public Class B
+    Inherits A
+
+    Public Overrides Iterator Function GetTs(Of t As New)() As IEnumerable(Of t)
+        For Each x In MyBase.GetTs(Of t)()
+            Yield x
+        Next
+    End Function
+
+End Class
+
+Public Class Tester
+
+End Class
